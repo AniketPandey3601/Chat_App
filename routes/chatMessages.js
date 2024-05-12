02a486fe96f82d79d69590df4155df654df1e0c7
unknown <you@example.com>
2024-05-12 17:33:57 +0530

    getting messages

--- routes/chatMessages.js
+++ routes/chatMessages.js
@@ -1,7 +1,7 @@
 // routes/chatMessages.js
 const express = require('express');
 const router = express.Router();
-
+const User = require('../models/User')
 const messages = require('../models/messages');
 const  authenticateToken  = require('../middleware/authenticateToken');
 
@@ -25,6 +25,34 @@ router.post('/sendMessage', authenticateToken, async (req, res) => {
     } catch (error) {
         console.error('Error storing chat message:', error);
         res.status(500).json({ message: 'Error storing chat message. Please try again later.' });
+    }
+});
+
+
+router.get('/getAllMessages', authenticateToken, async (req, res) => {
+    try {
+        // Fetch all chat messages from the database
+        // const allMessages = await messages.findAll({  include: [{
+        //     model: User,
+        //     attributes: ['name'], // Select only the 'name' attribute of the user
+        // }]}
+        // );
+
+
+        const allMessages = await messages.findAll({ include: User });
+
+        // const formattedMessages = allMessages.map(message => ({
+        //     username: message.user.name,
+        //     message: message.message
+        // }));
+
+        // // Send the list of messages as a response
+        // res.status(200).json({ messages: formattedMessages });
+
+        res.status(200).json({ messages: allMessages });
+    } catch (error) {
+        console.error('Error fetching chat messages:', error);
+        res.status(500).json({ message: 'Error fetching chat messages. Please try again later.' });
     }
 });
 
